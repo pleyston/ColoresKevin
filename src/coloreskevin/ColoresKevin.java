e3c40e94f283577a95b046fca906570217cc6cc1
pleyston <70038396+pleyston@users.noreply.github.com>
2021-12-06 21:34:41 -0300

    Update ColoresKevin.java

--- src/coloreskevin/ColoresKevin.java
+++ src/coloreskevin/ColoresKevin.java
@@ -8,10 +8,12 @@ public class ColoresKevin {
         String color1, color2, color3;
         color1 = "AZUL";
         color2 = "ROJO";
-        color3 = "VERDE";
+        color3 = "PURPURA";
+        color4 = "MAGENTA";
         System.out.printf("% es mi primer color favorito\n", color1);
-        System.out.printf("% es mi segundo color favorito\n", color2);
+        System.out.printf("% no es mi color principal pero tambien me gusta\n", color2);
         System.out.printf("% es mi tercer color favorito\n", color3);
+        System.out.printf("%es el ultimo color de la lista\n", color4);
     }
     
 }
