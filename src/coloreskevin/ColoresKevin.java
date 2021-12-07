d12f44070a87fdbddd30bc754bce489c25c3f42a
pleyston74 <dufourgkevin74@gmail.com>
2021-12-06 21:24:36 -0300

    primera versiO:n

---
+++ src/coloreskevin/ColoresKevin.java
@@ -0,0 +1,17 @@
+
+package coloreskevin;
+
+public class ColoresKevin {
+
+ 
+    public static void main(String[] args) {
+        String color1, color2, color3;
+        color1 = "AZUL";
+        color2 = "ROJO";
+        color3 = "VERDE";
+        System.out.printf("% es mi primer color favorito\n", color1);
+        System.out.printf("% es mi segundo color favorito\n", color2);
+        System.out.printf("% es mi tercer color favorito\n", color3);
+    }
+    
+}
